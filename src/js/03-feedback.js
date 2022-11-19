ab93bc027829496c78f848916973af9805d5eafd
Danik <95442609+BanduraDanik1@users.noreply.github.com>
2022-11-19 22:20:22 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+let dataImput = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
+
+form.addEventListener('submit', onSubmitForm);
+form.addEventListener('input', onImputForm);
+onSaveDataInput();
+
+function onImputForm() {
+  dataImput.email = email.value;
+  dataImput.message = message.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(dataImput));
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  console.log(dataImput);
+}
+
+function onSaveDataInput() {
+  const saveData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (saveData) {
+    email.value = saveData.email;
+    message.value = saveData.message;
+  }
+}
